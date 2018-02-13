dfd31d10fe6273704aa66c69b92ae1966ffd466e
kmcauliffe6 <mcauliffek@gatech.edu>
2018-02-13 15:33:42 -0500

    Initial commit

---
+++ app/src/main/java/edu/gatech/cs2340/thisteamnameapp/ApplicationActivity.java
@@ -0,0 +1,28 @@
+package edu.gatech.cs2340.thisteamnameapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ApplicationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_application);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void goToWelcomeActivity (View view){
+        Intent intent = new Intent (this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}

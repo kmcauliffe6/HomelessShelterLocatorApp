ade53d0df11fbaf17341b9d107971b1ab33c5b39
marianamatias <mmatias323@gmail.com>
2018-04-04 20:37:11 -0400

    Added spinner for ApplicationActivity to change language locale

---
+++ app/src/main/java/edu/gatech/cs2340/thisteamnameapp/AndroidLocalize.java
@@ -0,0 +1,83 @@
+package edu.gatech.cs2340.thisteamnameapp;
+
+/**
+ * Created by marianamatias on 4/4/18.
+ */
+
+        import java.util.Locale;
+        import android.os.Bundle;
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.content.res.Configuration;
+        import android.content.res.Resources;
+        import android.util.DisplayMetrics;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.ArrayAdapter;
+        import android.widget.Button;
+        import android.widget.Spinner;
+        import android.widget.Toast;
+        import android.widget.AdapterView.OnItemSelectedListener;
+
+public class AndroidLocalize extends Activity {
+//    Spinner spinnerctrl;
+//    Button btn;
+//    Locale myLocale;
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_application);
+//
+//        spinnerctrl = (Spinner) findViewById(R.id.spinner2);
+//        String[] arr = {"English", "Spanish", "French"};
+//        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, arr);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinnerctrl.setAdapter(adapter);
+//
+//        spinnerctrl.setOnItemSelectedListener(new OnItemSelectedListener() {
+//
+//            public void onItemSelected(AdapterView<?> parent, View view,
+//                                       int pos, long id) {
+//
+//                if (pos == 1) {
+//
+//                    Toast.makeText(parent.getContext(),
+//                            "You have selected English", Toast.LENGTH_SHORT)
+//                            .show();
+//                    setLocale("en");
+//                } else if (pos == 2) {
+//
+//                    Toast.makeText(parent.getContext(),
+//                            "You have selected Spanish", Toast.LENGTH_SHORT)
+//                            .show();
+//                    setLocale("es");
+//                } else if (pos == 3) {
+//
+//                    Toast.makeText(parent.getContext(),
+//                            "You have selected French", Toast.LENGTH_SHORT)
+//                            .show();
+//                    setLocale("fr");
+//                }
+//
+//            }
+//
+//            public void onNothingSelected(AdapterView<?> arg0) {
+//                // TODO Auto-generated method stub
+//            }
+//
+//        });
+//    }
+//
+//    public void setLocale(String lang) {
+//
+//        myLocale = new Locale(lang);
+//        Resources res = getResources();
+//        DisplayMetrics dm = res.getDisplayMetrics();
+//        Configuration conf = res.getConfiguration();
+//        conf.locale = myLocale;
+//        res.updateConfiguration(conf, dm);
+//        Intent refresh = new Intent(this, AndroidLocalize.class);
+//        startActivity(refresh);
+//    }
+}
--- app/src/main/java/edu/gatech/cs2340/thisteamnameapp/ApplicationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/thisteamnameapp/ApplicationActivity.java
@@ -1,19 +1,32 @@
 package edu.gatech.cs2340.thisteamnameapp;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Locale;
 
 /**
  * main screen with buttons for "see shelters" and "log out"
  */
 public class ApplicationActivity extends AppCompatActivity {
+
+    Spinner spinnerctrl;
+    Button btn;
+    Locale myLocale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,29 @@ public class ApplicationActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        spinnerctrl = (Spinner) findViewById(R.id.languagespinner);
+        String[] arr = {"English", "Spanish", "French"};
+        final ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, arr);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerctrl.setAdapter(adapter);
+
+        Button filterButton = findViewById(R.id.languagebutton);
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String type = spinnerctrl.getSelectedItem().toString(); //type from spinner
+                if (type.equals("English")) {
+                    setLocale("en");
+                } else if (type.equals("Spanish")) {
+                    setLocale("es");
+                } else if (type.equals("French")) {
+                    setLocale("fr");
+                }
+
+            }
+        });
+
     }
 
     public void goToWelcomeActivity (View view){
@@ -54,9 +90,17 @@ public class ApplicationActivity extends AppCompatActivity {
         }catch (IOException e) {
 
         }
+    }
 
+    public void setLocale(String lang) {
+        myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, AndroidLocalize.class);
+        startActivity(refresh);
+    }
 
-
-
-
-}}
+}
